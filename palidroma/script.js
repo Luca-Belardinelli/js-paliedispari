676dd07ac373fc3487d091c77ac82396bc75a5af
Luca Belardinelli <belardinelliluca@gmail.com>
2025-01-08 17:33:49 +0100

    [creation pariedispari]

---
+++ palidroma/script.js
@@ -0,0 +1,17 @@
+
+//Palidroma
+
+//Chiedere all’utente di inserire una parola
+
+//Creare una funzione per capire se la parola inserita è palindroma
+
+
+
+
+// chiediamo all'utente di inserire una parola
+
+let word = prompt("inserisci una parola");
+
+console.log(word);
+
+
