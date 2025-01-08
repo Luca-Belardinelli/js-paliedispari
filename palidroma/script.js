6639a9a4775fc74d6916b62f5562e68923507808
Luca Belardinelli <belardinelliluca@gmail.com>
2025-01-08 17:52:26 +0100

    [palidroma]

--- palidroma/script.js
+++ palidroma/script.js
@@ -3,10 +3,29 @@
 //Creare una funzione per capire se la parola inserita è palindroma
 
 
+// Chiedi all'utente di inserire una parola
+
+let user = prompt("Inserisci una parola per verificare se è palindroma:");
 
 
-let word = prompt("inserisci una parola");
+// Funzione per verificare se una parola è palindroma
+function palindrome(word) {
 
-console.log(word);
+    // Confronta la parola  invertita
+    
+    return word === word.split('').reverse().join('');
+
+}
 
 
+
+// Verifica se la parola è palindroma
+if (palindrome(user)) {
+
+    console.log('La parola è palindroma');
+
+} else {
+
+    console.log('La parola non è palindroma');
+
+}
