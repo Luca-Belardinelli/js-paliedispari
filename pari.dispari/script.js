1f88fe02357eb022553a60784942d8009aafc8a8
Luca Belardinelli <belardinelliluca@gmail.com>
2025-01-09 15:19:22 +0100

    [correction function]

--- pari.dispari/script.js
+++ pari.dispari/script.js
@@ -10,19 +10,21 @@
 
 const user = prompt(" Vuole essere pari o dispari?")
 
-if (user !== 'pari' && user !== 'dispari')
+if (user !== 'pari' && user !== 'dispari'){
 
-    console.log("Scelta non valida, Riprova.");
+    console.log("Scelta non valida, Riprova.")
+}
 
 //e inserisce un numero da 1 a 5
 
 const numberUser = parseInt(prompt("inserisca una numero da 1 a 5"))
 
-if (numberUser < 1 || numberUser > 5 )
+if (numberUser < 1 || numberUser > 5 ){
 
     console.log("Numero non valido, Inserisci un numero da 1 a 5.");
+}
 
-    console.log(numberUser);
+    console.log("il numero scelto è", numberUser);
 
 //Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 
@@ -32,6 +34,34 @@ function numberCpu() {
     
 }
 
-const numberestrattodalpc = numberCpu;
+const numberestrattodalpc = numberCpu();
 
-console.log("il numero della cpu è", numberCpu);
+console.log("il numero della cpu è", numberestrattodalpc);
+
+
+//Sommiamo i due numeri
+
+const sommanumeri = numberUser + numberestrattodalpc;
+
+console.log("la somma è",sommanumeri);
+
+
+//Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+
+function pari( sommanumeri) {
+
+    return sommanumeri % 2 === 0;
+
+}
+
+//Dichiariamo chi ha vinto.
+
+if ((sommanumeri && user === 'pari' ) || ( !sommanumeri && user === 'dispari')) {
+
+    console.log("hai vinto!");
+
+}else{
+
+    console.log('ritenta');
+
+}
